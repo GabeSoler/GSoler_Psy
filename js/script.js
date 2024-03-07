55b2969034a6d8301dd30c5d0a70aa95c42e4c97
Gabriel Soler <gsoler@gmail.com>
2024-03-07 10:12:23 +0000

    more content

--- js/script.js
+++ js/script.js
@@ -10,3 +10,29 @@ function navToggle() {
   }
 
 
+
+
+/* Creating a header that appears on scroll up */  
+  var headerDiv = document.querySelector("header");
+  var headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
+  var prevScrollpos = window.scrollY;
+
+
+  window.onscroll = function() {
+    var currentScrollPos = window.scrollY;
+  
+    /* if scrolling down, let it scroll out of view as normal */
+
+    if (prevScrollpos < currentScrollPos && currentScrollPos > headerBottom){
+        headerDiv.style.top ="-100px";
+    }
+    /* otherwise if we're scrolling up, fix the nav to the top */
+
+    else{  
+        headerDiv.style.top = '0';
+    }
+    
+    prevScrollpos = currentScrollPos;
+  } 
+
+
